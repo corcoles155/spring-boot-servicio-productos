3f8602fa3d574ab3d22e3f7bba34884f83a6f9cd
Ana Córcoles <ana.corcoles@gmail.com>
2019-11-01 19:10:20 +0100

    Add port field.

--- src/main/java/org/sanchez/corcoles/ana/pruebasconcepto/producto/model/entity/Producto.java
+++ src/main/java/org/sanchez/corcoles/ana/pruebasconcepto/producto/model/entity/Producto.java
@@ -20,6 +20,9 @@ public class Producto implements Serializable {
     @Temporal(TemporalType.DATE)
     private Date createAt;
 
+    @Transient
+    private Integer port;
+
     public Long getId() {
         return id;
     }
@@ -50,5 +53,13 @@ public class Producto implements Serializable {
 
     public void setCreateAt(Date createAt) {
         this.createAt = createAt;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
     }
 }
